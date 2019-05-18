db5884ebe5733f2d22ae12b3fa247fe2c26614b1
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-05-18 18:03:15 +0100

    :contsruction:
    User story 2 - Plane can takeoff
    Written test for airport class

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,8 @@ describe Airport do
   it 'can instruct a plane to land' do 
     expect(airport).to respond_to(:land).with(1).argument
   end
+
+  it 'can instruct a plane to takeoff' do 
+    expect(airport).to respond_to(:takeoff).with(1).argument
+  end
 end
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -7,4 +7,15 @@ describe 'User Stories' do
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
   end
+
+  
+# As an air traffic controller 
+# So I can get passengers on the way to their destination 
+# I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
+  it ' so a plane can takeoff, instruct plane to takeoff' do 
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.takeoff(plane) }.not_to raise_error
+  end
 end 
