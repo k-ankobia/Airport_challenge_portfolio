5b2a4aefe67dfde45a76ba8fda572ef05300696d
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-05-19 10:58:29 +0100

    :recycle: Refactored User Stories and tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,21 +2,23 @@ require 'airport'
 
 describe Airport do 
   let(:airport) { Airport.new }
+  let(:plane) { double :plane }
 
-
-  it 'can instruct a plane to land' do 
-    plane = double('it is a plane')
-    srand(100)
-    allow(airport).to receive(:stormy?).and_return false 
-    expect(airport.land(plane)).to eq [plane]
+  describe '#land' do 
+    it 'can instruct a plane to land' do 
+      srand(100)
+      allow(airport).to receive(:stormy?).and_return false 
+      expect(airport.land(plane)).to eq [plane]
+    end
   end
 
-    it 'can instruct a plane to takeoff' do 
-    plane = double('it is a plane')
-    srand(100)
-    allow(airport).to receive(:stormy?).and_return false 
-    airport.land(plane)
-    airport.takeoff
-    expect(airport.takeoff).to eq nil
+  describe '#takeoff' do
+  it 'can instruct a plane to takeoff' do 
+      srand(100)
+      allow(airport).to receive(:stormy?).and_return false 
+      airport.land(plane)
+      airport.takeoff
+      expect(airport.takeoff).to eq nil
+    end
   end
 end
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,10 +1,10 @@
 describe 'User Stories' do
+  let(:airport) {Airport.new}
+  let(:plane) { Plane.new}
   # As an air traffic controller
   # So planes can land safely at my airport
   # I would like to instruct a plane to land
   it 'so planes land at airports, instruct a plane to land' do
-    airport = Airport.new
-    plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
   end
 
@@ -14,20 +14,20 @@ describe 'User Stories' do
 # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
 
   it ' so a plane can takeoff, instruct plane to takeoff' do 
-    airport = Airport.new
     expect { airport.takeoff }.not_to raise_error
   end 
 
   it 'does not allow plane to land when weather is stormy' do 
-    airport = Airport.new
-    plane = Plane.new 
     allow(airport).to receive(:stormy?).and_return true 
     expect { airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy'
   end
 
   it 'does not allow plane to takeoff when weather is stormy' do 
-    airport = Airport.new
     allow(airport).to receive(:stormy?).and_return true 
     expect { airport.takeoff }.to raise_error 'Cannot takeoff plane: weather is stormy'
   end
+
+  it ' does not allow plane to land when airport is full' do 
+  end
+
 end
